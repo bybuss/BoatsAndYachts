94e7cb8e2ff72e7b3540001703e3cdf01b68edf4
bybuss <kukuhaha729@gmail.com>
2025-05-10 10:15:42 -0700

    Initial Commit

---
+++ BoatsAndYachts/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BoatsAndYachts
+//
+//  Created by Никита Светличный on 10.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
